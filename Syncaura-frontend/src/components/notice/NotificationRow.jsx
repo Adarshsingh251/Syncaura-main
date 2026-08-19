456a49de958d77e55d79ba0861bf687ebf25bf56
Baishnabi06 <baishnabimahapatra01gmail.com>
2026-08-19 20:37:55 +0530

    Fix submit notice in admin

--- Syncaura-frontend/src/components/notice/NotificationRow.jsx
+++ Syncaura-frontend/src/components/notice/NotificationRow.jsx
@@ -5,7 +5,12 @@ import { useTranslation } from "react-i18next";
 export default function NotificationRow({title, about, date, bgColor, docColor}) {
     const { t } = useTranslation();
     function formatDateYYYYMMDD(isoDate) {
+  if (!isoDate) return "N/A";
+
   const d = new Date(isoDate);
+
+  if (isNaN(d.getTime())) return "N/A";
+
   return d.toISOString().split("T")[0];
 }
 
